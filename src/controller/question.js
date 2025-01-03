6824e785dc3878a1586eed5fdf70b4e6a83cf5e7
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-03 15:05:07 +0200

    first part of questions finished

--- src/controller/question.js
+++ src/controller/question.js
@@ -22,4 +22,33 @@ const POST_QUESTION = async (req, res) => {
     return res.status(500).json({ message: "Something went wrong!" });
   }
 };
-export { POST_QUESTION };
+
+const GET_QUESTIONS = async (req, res) => {
+  try {
+    const questions = await QuestionModel.find();
+    return res.status(200).json({ questions: questions });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+const DELETE_QUESTION = async (req, res) => {
+  try {
+    const findOuestion = await QuestionModel.findOne({ id: req.params.id });
+    if (req.body.userId !== findOuestion.userId) {
+      return res.status(403).json({ message: "Access denied!" });
+    }
+    const questionToDel = await QuestionModel.findOneAndDelete({
+      id: req.params.id,
+    });
+    if (!questionToDel) {
+      return res.status(404).json({ message: "No such question exists!" });
+    }
+    return res.status(200).json({ message: "Question deleted successfully!" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+export { POST_QUESTION, GET_QUESTIONS, DELETE_QUESTION };
--- src/route/question.js
+++ src/route/question.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { POST_QUESTION } from "../controller/question.js";
+import {
+  POST_QUESTION,
+  GET_QUESTIONS,
+  DELETE_QUESTION,
+} from "../controller/question.js";
 import auth from "../utils/authorization.js";
 
 router.post("/question", auth, POST_QUESTION);
-
+router.get("/questions", GET_QUESTIONS);
+router.delete("/question/:id", auth, DELETE_QUESTION);
 export default router;
