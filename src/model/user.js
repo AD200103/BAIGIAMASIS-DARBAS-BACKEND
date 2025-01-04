a19d48743b0ec7425731a775157c7bfe471d07ab
Andrius200103 <cheekibreeki456845@gmail.com  git config --global user.name AD200103>
2025-01-04 12:07:56 +0200

    final commit maybe

--- src/model/user.js
+++ src/model/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   id: { type: String, required: true },
